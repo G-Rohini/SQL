3b9c34004d3d1b484fc978d048bcb6902e33fd8f
Rohini G <56411438+G-Rohini@users.noreply.github.com>
2020-02-13 17:20:52 -0800

    SQL Common Table Expression -CTE
    SQL Common Table Expression -CTE

---
+++ CTE.sql
@@ -0,0 +1,17 @@
+;with CTE as
+(select 
+[EmployeeID],
+max([TotalDue]) as MaxTotalDue 
+from 
+[Purchasing].[PurchaseOrderHeader]
+group by [EmployeeID]) 
+Select CTE.*, POH.[ShipDate], POH.[PurchaseOrderID]
+From [Purchasing].[PurchaseOrderHeader] POH
+Join CTE on POH.EmployeeID=CTE.EmployeeID
+AND CTE.MaxTotalDue=POH.TotalDue
+ORDER BY EmployeeID, ShipDate desc
+
+
+
+
+
